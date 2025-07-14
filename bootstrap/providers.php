dca836747ec77c0f71325e2f3b71f7f2d5ae50df
taufik7000 <mtaufikxxx@gmail.com>
2025-07-15 02:40:50 +0700

    main

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,9 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\BisnisPanelProvider::class,
+    App\Providers\Filament\DirekturPanelProvider::class,
     App\Providers\Filament\HrdPanelProvider::class,
+    App\Providers\Filament\KeuanganPanelProvider::class,
     App\Providers\Filament\TeamPanelProvider::class,
 ];
